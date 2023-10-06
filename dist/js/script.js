085877929223180073c170cada434cb29edff87c
Denis Denisov <denis.denisv8701@gmail.com>
2023-10-06 21:53:35 +0200

    created expirience + fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,19 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     close = document.querySelector('.menu__close');
+    overlay = document.querySelector('.menu__overlay');
+    choose = document.querySelector('#choose');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+choose.addEventListener('click', () => {
     menu.classList.remove('active');
 });
