2442c921d780a6bcdba020630a14e64049d3e743
Denis Denisov <denis.denisv8701@gmail.com>
2023-10-07 20:56:02 +0200

    skills , portfolio, price + fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     close = document.querySelector('.menu__close');
     overlay = document.querySelector('.menu__overlay');
-    choose = document.querySelector('#choose');
+    menuLinks = document.querySelectorAll('.menu-link1, .menu-link2, .menu-link3, .menu-link4, .menu-link5, .menu-link6');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -14,6 +14,16 @@ close.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
 });
-choose.addEventListener('click', () => {
-    menu.classList.remove('active');
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
+const procentes = document.querySelectorAll('.skills-ability__procente'),
+      indicatorSize = document.querySelectorAll('.skills-ability__indicator');
+
+procentes.forEach((procent, size) => {
+    indicatorSize[size].style.width = procent.innerHTML;
 });
